4cec59613e30bafbac3379aaff20e1e3edd8ea8f
Philip Putnam <putnamp989@gmail.com>
2017-03-03 10:32:53 -0800

    add content to main.html.twig, no bugs

--- app/app.php
+++ app/app.php
@@ -9,13 +9,19 @@
     * @backupStaticAttributes disabled
     */
 
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use($app) {
 
-        return $app['twig']->render('main.html.twig');
+
+        return $app['twig']->render('main.html.twig', array('stores' => Store::getAll()));
     });
 
 
