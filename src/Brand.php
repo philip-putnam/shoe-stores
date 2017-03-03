21e058dbbccefa75a182c9703abc9ce1fa75931d
Philip Putnam <putnamp989@gmail.com>
2017-03-03 08:55:26 -0800

    add save(), getAll(), deleteAll() function to Brand.php with passing tests in BrandTest.php

--- src/Brand.php
+++ src/Brand.php
@@ -24,6 +24,32 @@
         {
             $this->name = $new_name;
         }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+            $brands = [];
+
+            foreach($returned_brands as $brand)
+            {
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new Brand($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM brands;");
+        }
     }
 
 ?>
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -1,8 +1,23 @@
 <?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     require_once __DIR__.'/../src/Brand.php';
 
     class BrandTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+        {
+            Brand::deleteAll();
+        }
+
         function test_getId()
         {
             //Arrange
@@ -44,6 +59,67 @@
             //Assert
             $this->assertEquals('Nike', $result);
         }
+
+        function test_save()
+        {
+            //Arrange
+            $name = 'Puma';
+            $puma = new Brand($name);
+            $puma->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([$puma], $result);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = 'Puma';
+            $puma = new Brand($name);
+            $puma->save();
+
+            $name2 = 'Nike';
+            $nike = new Brand($name2);
+            $nike->save();
+
+            $name3 = 'Adidas';
+            $adidas = new Brand($name3);
+            $adidas->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([$puma, $nike, $adidas], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = 'Puma';
+            $puma = new Brand($name);
+            $puma->save();
+
+            $name2 = 'Nike';
+            $nike = new Brand($name2);
+            $nike->save();
+
+            $name3 = 'Adidas';
+            $adidas = new Brand($name3);
+            $adidas->save();
+
+            //Act
+            Brand::deleteAll();
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
+
     }
 
 
